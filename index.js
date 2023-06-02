b07c573d95c1a2e3183899aa8ca253c1081a1c31
Sudarshan4599 <130741404+Sudarshan4599@users.noreply.github.com>
2023-06-02 09:58:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// Toggle Icon Navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active')
+}
+
+
+
+// Scroll Section
+
+window.onscroll = () => {
+    let header = document.querySelector("header");
+    header.classList.toggle("sticky" , window.scrollY > 100);
+
+// Remove
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
